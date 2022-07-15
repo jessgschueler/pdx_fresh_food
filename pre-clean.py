dcb84e9a000d7d0ab89b8b0db03188e88429ebd9
jessgschueler <jessicagrace.schueler@gmail.com>
2022-07-15 14:09:14 -0700

    Add function using google API to add neighborhood data

--- pre-clean.py
+++ pre-clean.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import googlemaps
+
 
 def sextant_id(address):
+    """
+    Determine administrative sextant based on address
+    """
     add_list = address.split()
     sextant_list = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
     for i in add_list:
@@ -8,13 +13,31 @@ def sextant_id(address):
             return i
     return 'UNKNOWN'
 
+def api_call(address):
+    """
+    Call google maps api to determine neighborhood
+    """
+    API_KEY = #YOUR API KEY HERE
+    gmaps = googlemaps.Client(key=API_KEY)
+    return_dict = gmaps.geocode(address)
+    try:
+        neighborhood = return_dict[0]['address_components'][2]['long_name']
+        return neighborhood
+    except:
+        return 'UNKNOWN'
+
 def add_id(food_df, loc_col, path):
+    """
+    Add Portland Administrative Sextant to Datasets
+    """
+    df = pd.read_csv(path, error_bad_lines=False)
     food_df['sextant'] = food_df[loc_col].map(sextant_id)
     food_df.to_csv(path)
 
-
-def run(path, address):
+def add_neighborhood(food_df, loc_col, path):
+    """
+    Add neighborhood to datasets
+    """
     df = pd.read_csv(path, error_bad_lines=False)
-    add_id(df,address,path)
-
-run('data/CSA_Farm_Dropoff_Locations.csv', 'Location')
+    food_df['nighborhood'] = food_df[loc_col].map(api_call)
+    food_df.to_csv(path)
